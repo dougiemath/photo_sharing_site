5dd6ec499f2f429bd25244fdfac1887d321b7f99
dougiemath <douglasimatheson@gmail.com>
2022-02-25 15:09:11 +0000

    added comment and post admin models

--- pss_app/admin.py
+++ pss_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from pss_app.models import Post
+from pss_app.models import Post, Comment
 
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on')
