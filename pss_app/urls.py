204292e97b28dc7102cbff5103a7aa92cfd68240
dougiemath <douglasimatheson@gmail.com>
2022-02-25 17:26:20 +0000

    committing with bug - failed to connect model+view

--- pss_app/urls.py
+++ pss_app/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name ="homepage"),
+    # path('', views.PostList.as_view(), name ="home"),
     path('register/', views.register, name ="register"),
     path('', include("django.contrib.auth.urls")),
 
