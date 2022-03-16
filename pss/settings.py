f2da009f4634078fc45a0fa49fa34f4174f883b2
dougiemath <douglasimatheson@gmail.com>
2022-03-16 22:00:49 +0000

    allauth email attempt 3

--- pss/settings.py
+++ pss/settings.py
@@ -168,11 +168,12 @@ ACCOUNT_LOGOUT_REDIRECT_URL ='/'
 # redirects to profile page if not configured.
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
